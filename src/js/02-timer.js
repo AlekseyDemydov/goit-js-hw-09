289a568351dda623b2beea4f3cf3607ab33d9921
AlekseyDemydov <96746949+AlekseyDemydov@users.noreply.github.com>
2022-07-05 17:10:35 +0300

    update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,15 +2,17 @@
 import flatpickr from 'flatpickr';
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const btnStart = document.querySelector('[data-start]');
 const daysEl = document.querySelector('[data-days]');
 const hoursEl = document.querySelector('[data-hours]');
 const minutesEl = document.querySelector('[data-minutes]');
 const secondsEl = document.querySelector('[data-seconds]');
+const inputEl = document.querySelector('#datetime-picker');
 
 let selected = null;
-
+btnStart.disabled = true;
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -21,21 +23,32 @@ const options = {
     selected = selectedDates[0].getTime();
 
     if (selected < new Date()) {
-      alert('Please choose a date in the future');
+      Notify.failure('Please choose a date in the future');
+      btnStart.disabled = true;
       return;
     }
-    btnStart.disabbled = false;
+    btnStart.disabled = false;
   },
 };
+function addZero(value) {
+  return String(value).padStart(2, '0');
+}
 const foo = () => {
-  setInterval(() => {
-    btnStart.disabbled = true;
+  const fr = setInterval(() => {
+    inputEl.disabled = true;
+    btnStart.disabled = true;
     const diff = selected - Date.now();
+    console.log(diff);
+    if (diff < 1000) {
+      clearInterval(fr);
+      inputEl.disabled = false;
+      btnStart.disabled = true;
+    }
     const { days, hours, minutes, seconds } = convertMs(diff);
-    daysEl.textContent = days;
-    hoursEl.textContent = hours;
-    minutesEl.textContent = minutes;
-    secondsEl.textContent = seconds;
+    daysEl.textContent = addZero(days);
+    hoursEl.textContent = addZero(hours);
+    minutesEl.textContent = addZero(minutes);
+    secondsEl.textContent = addZero(seconds);
   }, 1000);
 };
 let calendar = flatpickr('#datetime-picker', options);
